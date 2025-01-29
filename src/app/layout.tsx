09eaccbcb8022c0920cdc7080ea74fc14a159f43
yousof <yosofasady2@gmail.com>
2025-01-29 07:04:23 +0330

    refactor: some issues were fixed inside home page an layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,39 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import { ReactNode } from 'react';
+import MainLayout from '@/components/layout/MainLayout';
 
-export const metadata: Metadata = {
-  title: "Lotch",
-  description: "",
-};
+async function getSiteSettings() {
+  const res = await fetch(`http://localhost:8002/api-v1/site-settings/`);
+  if (!res.ok) throw new Error('Failed to fetch settings');
+  return res.json();
+}
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function RootLayout({ children }: { children: ReactNode }) {
+  const siteSettings = await getSiteSettings();
+
   return (
-    <html lang="fa" dir="rtl">
-
-      <title>Lotch Shop</title>
-      <meta name="description" content="Y.A.A" />
-      <meta name="author" content="Y.A.A" />
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>TB_TG Shop</title>
+        <meta name="description" content="Y.A.A" />
+        <meta name="author" content="FLATLIFE" />
+        <link rel="stylesheet" href="/css/linearicons.css" />
+        <link rel="stylesheet" href="/css/owl.carousel.css" />
+        <link rel="stylesheet" href="/css/themify-icons.css" />
+        <link rel="stylesheet" href="/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="/css/nice-select.css" />
+        <link rel="stylesheet" href="/css/nouislider.min.css" />
+        <link rel="stylesheet" href="/css/bootstrap.css" />
+        <link rel="stylesheet" href="/css/main.css" />
+      </head>
 
       <body>
-        {children}
+        <MainLayout siteSettings={siteSettings[0]}>
+
+          {children}
+
+        </MainLayout>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,14 @@
 import React from 'react';
-import BannerArea from '@/components/home/BannerArea'
-import FeaturesArea from '@/components/home/FeaturesArea'
-import LatestProducts from '@/components/home/LatestProducts'
-import BestSalesProducts from '@/components/home/BestSalesProducts'
-import apiClient from '@/services/apiClient'
 
-interface Banner {
-  product: {
-    title: string;
-    description: string;
-    images: { image: string }[];
-  }[];
-}
 
-interface Product {
-  id: number;
-  title: string;
-  price: number;
-  org_price: number;
-  images: { image: string }[];
-}
 
-interface HomePageProps {
-  banners: Banner[];
-  latestProducts: Product[];
-  bestSalesProducts: Product[];
-}
 
-const HomePage: React.FC<HomePageProps> = ({ banners, latestProducts, bestSalesProducts }) => {
+const Home = () => {
   return (
-    <div>
-      <BannerArea banners={banners} />
-      <FeaturesArea />
-      <LatestProducts products={latestProducts} />
-      <BestSalesProducts products={bestSalesProducts} />
-    </div>
+    <HomePage />
+
   );
 };
 
-const getServerSideProps = async () => {
-  try {
-    const [bannersResponse, latestProductsResponse, bestSalesProductsResponse] = await Promise.all([
-      apiClient.get('/home-banners/'),
-      apiClient.get('/products/', { params: { ordering: '-updated_at' } }),
-      apiClient.get('/products/', { params: { ordering: 'best_sales' } }),
-    ]);
+export default Home;
 
-    return {
-      props: {
-        banners: bannersResponse.data,
-        latestProducts: latestProductsResponse.data.results,
-        bestSalesProducts: bestSalesProductsResponse.data.results,
-      },
-    };
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return {
-      props: {
-        banners: [],
-        latestProducts: [],
-        bestSalesProducts: [],
-      },
-    };
-  }
-};
-
-export default HomePage;
