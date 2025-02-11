6c3073d4ff86b7b3ead9f9ba2a8bef4804ced7a4
yousof <yosofasady2@gmail.com>
2025-02-11 15:02:05 +0330

    refactor: blog detail redesign

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,12 +1,13 @@
-import { notFound } from 'next/navigation';
-import BannerArea from '@/components/base/Banner';
-import CommentsSection from '@/components/blog/CommentSectoins';
-import apiClient from '@/services/apiClient';
-import BlogContent from '@/components/blog/BlogContent';
+import { notFound } from "next/navigation";
+import BannerArea from "@/components/base/Banner";
+import CommentsSection from "@/components/blog/CommentSectoins";
+import apiClient from "@/services/apiClient";
+import BlogContent from "@/components/blog/BlogContent";
+import BlogBanner from "@/components/blog/BlogBanner";
 
 export async function generateStaticParams() {
   const fetchAllBlogIds = async () => {
-    const response = await apiClient.get('/blog/blogs/');
+    const response = await apiClient.get("/blog/blogs/");
     return response.data.results.map((blog: { id: string }) => ({
       id: blog.id.toString(),
     }));
@@ -15,14 +16,17 @@ export async function generateStaticParams() {
   try {
     return await fetchAllBlogIds();
   } catch (error) {
-    console.error('Error generating static paths:', error);
+    console.error("Error generating static paths:", error);
     return [];
   }
 }
 
 export default async function BlogDetailPage({
-  params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   const fetchBlogDetails = async () => {
     const response = await apiClient.get(`/blog/blogs/${id}/`);
     return response.data;
@@ -43,17 +47,17 @@ export default async function BlogDetailPage({
 
     return (
       <>
-        <BannerArea
-        />
-
-        <BlogContent blog={blogDetails} />
+        <BlogBanner />
+        <div className="flex justify-center lg:px-64 md:px-40 sm:px-20 px-10 text-black">
+          <BlogContent blog={blogDetails} />
+        </div>
         <CommentsSection blogId={id} initialComments={comments} />
       </>
     );
   } catch (error) {
-    console.error('Error fetching blog data:', error);
+    console.error("Error fetching blog data:", error);
     return notFound();
   }
 }
 
-export const revalidate = 3600; 
+export const revalidate = 3600;
--- src/components/blog/BlogContent.tsx
+++ src/components/blog/BlogContent.tsx
@@ -1,62 +1,57 @@
-import { Blog } from '@/lib/blog';
+import { Blog } from "@/lib/blog";
+import Link from "next/link";
+import { IoMdEye } from "react-icons/io";
+import { MdMessage } from "react-icons/md";
+import { SlCalender } from "react-icons/sl";
 
 export default function BlogContent({ blog }: { blog: Blog }) {
-	const formatDate = (dateString: string) => {
-		const options: Intl.DateTimeFormatOptions = {
-			year: 'numeric',
-			month: 'short',
-			day: 'numeric',
-		};
-		return new Date(dateString).toLocaleDateString(undefined, options);
-	};
-
-	return (
-		<section className="blog_area single-post-area section_gap">
-			<div className="container">
-				<div className="row">
-					<div className="col-lg-12 posts-list">
-						<div className="single-post row">
-							<div className="col-lg-12">
-								<div className="feature-img">
-									<img
-										src={blog.thumbnail}
-										alt={blog.title}
-										width={1200}
-										height={600}
-										className="img-fluid"
-									/>
-								</div>
-							</div>
-							<div className="col-lg-3 col-md-3">
-								<div className="blog_info text-right">
-									<ul className="blog_meta list">
-										<li className="text-capitalize">
-											<i className="lnr lnr-user"></i>
-											{`${blog.author.first_name} ${blog.author.last_name}`}
-										</li>
-										<li>
-											<i className="lnr lnr-calendar-full"></i>
-											{formatDate(blog.updated_at)}
-										</li>
-										<li>
-											<i className="lnr lnr-eye"></i>
-											{`${blog.views} Views`}
-										</li>
-										<li>
-											<i className="lnr lnr-bubble"></i>
-											{`${blog.comments_count} Comments`}
-										</li>
-									</ul>
-								</div>
-							</div>
-							<div className="col-lg-9 col-md-9 blog_details">
-								<h2>{blog.title}</h2>
-								<p className="excert">{blog.body}</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <article key={blog.id} className="bg-gray-200 p-16">
+      <div className="">
+        <div className="">
+          <ul className="">
+            <Link href={`/blogs/${blog.id}`}>
+              <h2 className="text-5xl font-bold">{blog.title}</h2>
+            </Link>
+            <div className="my-4 text-md">
+              <div className="flex gap-2">
+                <li className="flex gap-3 items-center">
+                  <SlCalender />
+                  {new Date(blog.updated_at).toLocaleDateString(undefined, {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  })}
+                </li>
+                <p>by</p>
+                <li className="capitalize text-neutral">
+                  {blog.author.first_name} {blog.author.last_name}
+                </li>
+              </div>
+              <li className="flex gap-3 items-center">
+                <IoMdEye />
+                {blog.views} {"views"}
+              </li>
+              <li className="flex gap-3 items-center">
+                <MdMessage />
+                {blog.comments_count} {"comments"}
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+      <div className="">
+        <div className="">
+          <div className="">
+            <Link href={`/blogs/${blog.id}`} className="genric-btn primary">
+              <img src={blog.thumbnail} alt={blog.title} className="" />
+            </Link>
+          </div>
+          <div>
+            <p className=" tracking-widest my-8">{blog.body}</p>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 }
