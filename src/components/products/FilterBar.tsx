e620ac140523abb0f15e610a275c9c8b9c0f4f05
yousof <yosofasady2@gmail.com>
2025-02-14 19:46:46 +0330

    refactor: fix some design issues in filter bar

--- src/components/products/FilterBar.tsx
+++ src/components/products/FilterBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useTranslation } from "react-i18next";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { IoChevronDownOutline } from "react-icons/io5";
 
 interface FilterBarProps {
   selectedProducts: string[];
@@ -25,26 +26,77 @@ export default function FilterBar({
     const newValue = appliedFilters.secondHand === value ? null : value;
     onFilterChange({ secondHand: newValue });
   };
-
+  console.log({ appliedFilters });
   return (
     <div className="flex flex-col md:flex-row gap-4 items-center justify-between bg-base-200 p-4 rounded-none mb-4">
       <div className="flex flex-wrap gap-2">
-        <div className="form-control rounded-none">
-          <select
-            className="select select-bordered select-sm rounded-none"
-            value={appliedFilters.ordering || "default"}
-            onChange={(e) => handleSortingChange(e.target.value)}
+        <div className="dropdown">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-sm bg-black text-white border border-base-300 rounded-none"
           >
-            <option value="default">{t("Default sorting")}</option>
-            <option value="price_asc">{t("Price: Low to High")}</option>
-            <option value="price_desc">{t("Price: High to Low")}</option>
-            <option value="best_sales">{t("Best Sales")}</option>
-          </select>
+            {appliedFilters.ordering === "price_asc" && t("Price: Low to High")}
+            {appliedFilters.ordering === "price_desc" &&
+              t("Price: High to Low")}
+            {appliedFilters.ordering === "best_sales" && t("Best Sales")}
+            {appliedFilters.ordering === "default" && t("Default sorting")}
+            {!appliedFilters.ordering && t("Default sorting")}
+            <IoChevronDownOutline />
+          </div>
+          <ul className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-none border border-base-300 w-52">
+            <li>
+              <button
+                className={`text-sm ${
+                  !appliedFilters.ordering ? "active rounded-none" : ""
+                }`}
+                onClick={() => handleSortingChange("default")}
+              >
+                {t("Default sorting")}
+              </button>
+            </li>
+            <li>
+              <button
+                className={`text-sm ${
+                  appliedFilters.ordering === "price_asc"
+                    ? "active rounded-none"
+                    : ""
+                }`}
+                onClick={() => handleSortingChange("price_asc")}
+              >
+                {t("Price: Low to High")}
+              </button>
+            </li>
+            <li>
+              <button
+                className={`text-sm ${
+                  appliedFilters.ordering === "price_desc"
+                    ? "active rounded-none"
+                    : ""
+                }`}
+                onClick={() => handleSortingChange("price_desc")}
+              >
+                {t("Price: High to Low")}
+              </button>
+            </li>
+            <li>
+              <button
+                className={`text-sm ${
+                  appliedFilters.ordering === "best_sales"
+                    ? "active rounded-none"
+                    : ""
+                }`}
+                onClick={() => handleSortingChange("best_sales")}
+              >
+                {t("Best Sales")}
+              </button>
+            </li>
+          </ul>
         </div>
 
-        <div className="join rounded-none">
+        <div className="join rounded-none ">
           <button
-            className={`join-item btn btn-sm ${
+            className={`join-item btn btn-sm border-black border-1 ${
               appliedFilters.secondHand === true ? "bg-black text-white" : ""
             }`}
             onClick={() => handleSecondHandChange(true)}
@@ -52,8 +104,10 @@ export default function FilterBar({
             {t("Used")}
           </button>
           <button
-            className={`join-item btn btn-sm ${
-              appliedFilters.secondHand === false ? "bg-black text-white" : ""
+            className={`join-item btn btn-sm border-black border-1 ${
+              appliedFilters.secondHand === false || !appliedFilters.secondHand
+                ? "bg-black text-white"
+                : ""
             }`}
             onClick={() => handleSecondHandChange(false)}
           >
